c02dfe2706c7ac86b09c7e0b88936ead1f391fdb
Kononiuk <hmasteraaa@gmail.com>
2024-11-05 23:02:26 +0200

    fixed vite

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,10 @@
 import { Route, Routes } from "react-router-dom";
+import Home1 from "./Components/Home1";
+import Home2 from "./Components/Home2";
+import Navbar from "./Components/Navbar";
+import AnimationPart from "./Components/AnimationPart";
+import BigSection from "./Components/BigSection";
+import Toke from "./Components/Toke";
 import { useEffect } from "react";
 import AOS from "aos";
 import Main from "./Components/main";
