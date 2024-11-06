7228749d7913e4ad535a4a66f5704cb290cc0139
Kononiuk <hmasteraaa@gmail.com>
2024-11-06 11:08:14 +0200

    fixed ci

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
 import vercel from 'vite-plugin-vercel';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vercel()],
-})
+   plugins: [vercel()],
+   build: {
+      outDir: 'dist'
+   }
+});
